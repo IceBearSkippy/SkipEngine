0a321477e1312a2d627d4820056c27a844be7e0d
Sanjay Sagar <icebearskippy@gmail.com>
2020-06-05 20:00:19 -0700

    Add glfw loop to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,38 @@
-﻿
-#include <VulkanManager.h>;
+﻿#include <VulkanManager.h>
+#include <VulkanWindow.h>
 
-#include <iostream>
 using namespace std;
 
-#ifdef NDEBUG
-const bool enableValidationLayers = false;
-#else
-const bool enableValidationLayers = true;
-#endif
-
 Skip::VulkanWindow* window;
-Skip::VulkanManager* vulkan;
+Skip::VulkanManager* vulkanManager;
 int main()
 {
 	// Main is our application
 	// We will interact with Vulkan manager and vulkan window
 	// GLFW will live in VulkanWindow and have public access
 
-	cout << "Hello CMake." << endl;
+	// create window
+	window = new Skip::VulkanWindow();
+	window->init();
+
+
+	// create VulkanManager
+	vulkanManager = new Skip::VulkanManager(window);
+	
+	//setup validation layers
+	#ifndef NODEBUG
+		vulkanManager->_enableValidationLayers = true;
+	#endif
+	vulkanManager->_validationLayers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
+	vulkanManager->init();
+	while (!window->shouldClose()) {
+		//glfwPollEvents();
+		//drawFrame()
+	}
+
+	vulkanManager->~VulkanManager();
 	return 0;
 }
