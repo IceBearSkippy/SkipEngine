b3cd741bad57111f4a023dda2fdebc3a08958217
Sanjay Sagar <icebearskippy@gmail.com>
2020-06-05 20:00:02 -0700

    Initialized and cleanup a vulkan instance

--- include/VulkanManager.h
+++ include/VulkanManager.h
@@ -1,18 +1,48 @@
+#pragma once
+
 #include <vulkan/vulkan.h>
 
-#include <VulkanWindow.h>
+#include <string>
+#include <optional>
+#include <vector>
+#include <iostream>
+
+
+const std::string ENGINE_NAME = "SkipEngine";
+const uint32_t ENGINE_VERSION = VK_MAKE_VERSION(1, 2, 1);
+
+const uint32_t ENGINE_API_VERSION = VK_API_VERSION_1_2;
 
 namespace Skip {
 
+	class VulkanWindow;
+
+	struct QueueFamilyIndices {
+		std::optional<uint32_t> graphicsFamily;
+		std::optional<uint32_t> presentFamily;
+		bool isComplete();
+	};
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR& surface);
+
 	class VulkanManager {
 	public:
-		VulkanManager(VulkanWindow window);
+		VulkanManager(VulkanWindow* window);
 		~VulkanManager();
 
+		void init();
+		bool checkValidationLayerSupport();
+
+		VulkanWindow* _window;
+		bool _enableValidationLayers;
+		
+		std::vector<const char*> _validationLayers;
+		std::vector<const char*> getRequiredExtensions();
+		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 
 	private:
-		VkInstance instance;
-		VulkanWindow vkWindow;
+		void createInstance();
+		VkInstance _instance = VK_NULL_HANDLE;
 	};
 
 
--- src/VulkanManager.cpp
+++ src/VulkanManager.cpp
@@ -1,10 +1,127 @@
-#include <VulkanManager.h>;
+#include <VulkanManager.h>
+#include <VulkanWindow.h>
 
 namespace Skip {
-	VulkanManager::VulkanManager(VulkanWindow window) {
-		
+
+	VulkanManager::VulkanManager(VulkanWindow* window) {
+        _window = window;
+        _enableValidationLayers = false;
+        _instance = VK_NULL_HANDLE;
 	};
-	VulkanManager::~VulkanManager() {
-		
+    void VulkanManager::init() {
+        // create vulkan instance
+        this->createInstance();
+    };
+    VulkanManager::~VulkanManager() {
+        vkDestroyInstance(_instance, nullptr);
+        _window->~VulkanWindow();
 	};
+
+    bool VulkanManager::checkValidationLayerSupport() {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName : _validationLayers) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    std::vector<const char*> VulkanManager::getRequiredExtensions() {
+        //GLFW extensions!
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        if (_enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+        return extensions;
+    }
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData) {
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+        return VK_FALSE;
+
+    }
+
+    void VulkanManager::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+        createInfo = {};
+        createInfo.sType =
+            VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity =
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
+            | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+        createInfo.pUserData = nullptr; // Optional
+    }
+
+    void VulkanManager::createInstance() {
+        if (_enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available");
+        }
+        // instance is connection between app and Vulkan library
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = ENGINE_VERSION; // is this the version?
+        appInfo.pEngineName = ENGINE_NAME.c_str();
+        appInfo.engineVersion = ENGINE_VERSION;
+        appInfo.apiVersion = ENGINE_API_VERSION;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = getRequiredExtensions();
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(glfwExtensions.size());
+        createInfo.ppEnabledExtensionNames = glfwExtensions.data();
+
+
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
+        if (_enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(_validationLayers.size());
+            createInfo.ppEnabledLayerNames = _validationLayers.data();
+
+            populateDebugMessengerCreateInfo(debugCreateInfo);
+            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+        } else {
+            createInfo.enabledLayerCount = 0;
+
+            createInfo.pNext = nullptr;
+        }
+
+        if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    
+
+    
+    
 }
