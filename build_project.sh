5417943d823733a11fc9929849742f4950dcea26
Sanjay Sagar <icebearskippy@gmail.com>
2020-07-02 15:05:31 -0700

    Add commands to build executable

--- build_project.sh
+++ build_project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source source-env.sh
-cmake -DCMAKE_C_COMPILER_ID=gcc -DCMAKE_CXX_COMPILER_ID=g++ . && make
+source source-env.sh;
+cmake -DCMAKE_C_COMPILER_ID=gcc -DCMAKE_CXX_COMPILER_ID=g++ -B./build;
 
-
+cd build;
+make;
