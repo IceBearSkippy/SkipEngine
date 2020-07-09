c873f02a5c3653a7b512ee4a91101dafe753dc86
Sanjay Sagar <icebearskippy@gmail.com>
2020-07-08 18:35:47 -0700

    Add surface normals and vertex buffer drawing

--- include/objects/Cube.h
+++ include/objects/Cube.h
@@ -3,48 +3,56 @@
 #include <stdexcept>
 
 const std::vector<Skip::Vertex> CUBE_VERTICES = {
-    // these are blender stuffs
+
+    // vertex, color, texCoord, normal
+
     // + y (faces up)
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.875f, 0.50f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.875f, 0.25f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.875f, 0.25f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f)},
-
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f), glm::vec3(0.0f, 1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.875f, 0.50f), glm::vec3(0.0f, 1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.875f, 0.25f), glm::vec3(0.0f, 1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.875f, 0.25f), glm::vec3(0.0f, 1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f), glm::vec3(0.0f, 1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f), glm::vec3(0.0f, 1.0f, 0.0f)},
+    
     // + z (faces towards screen)
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.0f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.0f)},
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f), glm::vec3(0.0f, 0.0f, 1.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f), glm::vec3(0.0f, 0.0f, 1.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f), glm::vec3(0.0f, 0.0f, 1.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)},
 
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 1.0f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 1.0f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.75f)},
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 1.0f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.75f)},
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.75f)},
+    // - x (left)
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.75f), glm::vec3(-1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.75f), glm::vec3(-1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.75f), glm::vec3(-1.0f, 0.0f, 0.0f)},
 
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.125f, 0.50f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f)},
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.125f, 0.50f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f)},
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.125f, 0.25f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f)},
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.75f)},
-    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.75f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f)},
-    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.75f)},
-    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f)},
-    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f)}
+    // - y (bottom)
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.125f, 0.50f), glm::vec3(0.0f, -1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f), glm::vec3(0.0f, -1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f), glm::vec3(0.0f, -1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.125f, 0.50f), glm::vec3(0.0f, -1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f), glm::vec3(0.0f, -1.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.125f, 0.25f), glm::vec3(0.0f, -1.0f, 0.0f)},
+
+    // + x (right)
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f), glm::vec3(1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f), glm::vec3(1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f), glm::vec3(1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f), glm::vec3(1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.25f), glm::vec3(1.0f, 0.0f, 0.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.25f), glm::vec3(1.0f, 0.0f, 0.0f)},
+    
+    // - z (faces away screen)
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.75f), glm::vec3(0.0f, 0.0f, -1.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.75f), glm::vec3(0.0f, 0.0f, -1.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f), glm::vec3(0.0f, 0.0f, -1.0f)},
+    Skip::Vertex {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.75f), glm::vec3(0.0f, 0.0f, -1.0f)},
+    Skip::Vertex {glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.625f, 0.50f), glm::vec3(0.0f, 0.0f, -1.0f)},
+    Skip::Vertex {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.375f, 0.50f), glm::vec3(0.0f, 0.0f, -1.0f)}
 };
 namespace Skip {
 
--- src/objects/Cube.cpp
+++ src/objects/Cube.cpp
@@ -16,11 +16,15 @@ namespace Skip {
 
     void Cube::loadObject() {
         for (auto vertex : CUBE_VERTICES) {
-            if (_uniqueVertices.count(vertex) == 0) {
-                _uniqueVertices[vertex] = static_cast<uint32_t>(_vertices.size());
+            if (_useIndexBuffer) {
+                if (_uniqueVertices.count(vertex) == 0) {
+                    _uniqueVertices[vertex] = static_cast<uint32_t>(_vertices.size());
+                    _vertices.push_back(vertex);
+                }
+                _indices.push_back(_uniqueVertices[vertex]);
+            } else {
                 _vertices.push_back(vertex);
             }
-            _indices.push_back(_uniqueVertices[vertex]);
         }
     }
 }
