ed99ff2f768a69d1fb4516e29ccda6f4e736e60a
Sanjay Sagar <icebearskippy@gmail.com>
2020-07-13 19:03:58 -0700

    Add components back to demo lighting

--- src/main.cpp
+++ src/main.cpp
@@ -32,16 +32,16 @@ int main()
         "resources/models/viking_room.obj",
         true
     );
-    //skipObjects.push_back(modelObject);
+    skipObjects.push_back(modelObject);
     Skip::Cube* cube = new Skip::Cube(
         glm::vec3(2.0f, 1.0f, 0.0f), DEFAULT_TEXTURE, false
     );
     skipObjects.push_back(cube);
 
     Skip::Sphere* sphere = new Skip::Sphere(
-        glm::vec3(-2.0f, 1.0f, 0.0f), 12, DEFAULT_TEXTURE, false
+        glm::vec3(2.0f, 1.5f, 0.0f), 12, DEFAULT_TEXTURE, false
     );
-    //skipObjects.push_back(sphere);
+    skipObjects.push_back(sphere);
 
     // create window
     // Window will create keys to events based on components
@@ -54,6 +54,7 @@ int main()
     uint32_t currentImage;
     float currentTime, deltaTime;
     float lastTime = 0.0;
+    glm::mat4 mvpMat;
     while (!window->shouldClose()) {
         glfwPollEvents();
 
@@ -64,24 +65,28 @@ int main()
 
         window->processKeys(deltaTime);
 
-        /*modelObject->_mvpUBO.model = Skip::buildRotateX(glm::radians(90.0f));
+        modelObject->_mvpUBO.model = Skip::buildRotateX(glm::radians(90.0f));
         modelObject->_mvpUBO.view = camera->GetViewMatrix();
         modelObject->_mvpUBO.proj = glm::perspective(glm::radians(45.0f), swapchain->_swapChainExtent.width / (float)swapchain->_swapChainExtent.height, 0.1f, 10.0f);
-        modelObject->_mvpUBO.proj[1][1] *= -1;*/
+        modelObject->_mvpUBO.proj[1][1] *= -1;
+
         cube->_mvpUBO.model = cube->GetPositionMatrix() * Skip::buildScale(0.2f, 0.2f, 0.2f);
         cube->_mvpUBO.view = camera->GetViewMatrix();
         cube->_mvpUBO.proj = glm::perspective(glm::radians(45.0f), swapchain->_swapChainExtent.width / (float)swapchain->_swapChainExtent.height, 0.1f, 10.0f);
         cube->_mvpUBO.proj[1][1] *= -1;
+        mvpMat = cube->_mvpUBO.proj * cube->_mvpUBO.view * cube->_mvpUBO.model;
+        cube->_mvpUBO.norm = glm::transpose(glm::inverse((mvpMat)));
 
-        cube->_lightUBO.ambient = glm::vec4(0.2473f, 0.1995f, 0.0745f, 1.0f);
-        cube->_lightUBO.diffuse = glm::vec4(0.751f, 0.6065f, 0.2265f, 1.0f);
-        cube->_lightUBO.specular = glm::vec4(0.6283f, 0.5559f, 0.3661f, 1.0f);
-        cube->_lightUBO.position = glm::vec3(0.0f, 0.0f, 0.0f);
-
-        /*sphere->_mvpUBO.model = sphere->GetPositionMatrix() * Skip::buildScale(0.2f, 0.2f, 0.2f);
+        sphere->_mvpUBO.model = sphere->GetPositionMatrix() * Skip::buildScale(0.2f, 0.2f, 0.2f);
         sphere->_mvpUBO.view = camera->GetViewMatrix();
         sphere->_mvpUBO.proj = glm::perspective(glm::radians(45.0f), swapchain->_swapChainExtent.width / (float)swapchain->_swapChainExtent.height, 0.1f, 10.0f);
-        sphere->_mvpUBO.proj[1][1] *= -1;*/
+        sphere->_mvpUBO.proj[1][1] *= -1;
+        sphere->_lightUBO.ambient = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f) * 100.0f;
+        sphere->_lightUBO.diffuse = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f) * 10.0f;
+        sphere->_lightUBO.specular = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f) * 10.0f;
+
+        mvpMat = sphere->_mvpUBO.proj * sphere->_mvpUBO.view * sphere->_mvpUBO.model;
+        sphere->_mvpUBO.norm = glm::transpose(glm::inverse((mvpMat)));
         
         swapchain->updateUniformBuffers(currentImage);
 
